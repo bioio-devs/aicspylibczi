c8d517edd48f0105096e4474e8158acebea01442
Brian Whitney <brian.whitney@alleninstitute.org>
2026-09-18 11:26:07 -0700

    Bump version: 4.0.0 → 4.0.1

--- setup.py
+++ setup.py
@@ -118,7 +118,7 @@ setup(
     name="aicspylibczi",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.md
-    version="4.0.0",
+    version="4.0.1",
     author="Jamie Sherman, Paul Watkins",
     author_email="jamies@alleninstitute.org, pwatkins@gmail.com",
     description="A python module and a python extension for Zeiss (CZI/ZISRAW) microscopy files.",
